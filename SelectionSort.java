6b19224181c2a12d1b08ce788e19f4970fb8ee65
Ayash <aliayash@UMICH.EDU>
2023-12-14 15:54:50 -0500

    Initial commit of files

---
+++ SelectionSort.java
@@ -0,0 +1,36 @@
+
+public class SelectionSort {
+    
+    public void selectionSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        printArray("Original array: ", arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                swap(arr, i, minIndex);
+            }
+        }
+        printArray("Sorted array: ", arr);
+    }
+
+    private void swap(int[] arr, int i, int minIndex) {
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    private void printArray(String message, int[] arr) {
+        System.out.print(message + "[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + (i < arr.length - 1 ? ", " : ""));
+        }
+        System.out.println("]");
+    }
+}
---
+++ SelectionSortTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+public class SelectionSortTest {
+
+    @Test
+    public void testPositive() {
+        SelectionSort sorter = new SelectionSort();
+        int[] actual = {5, 3, 8, 4, 2};
+        sorter.selectionSort(actual);
+        int[] expected = {2, 3, 4, 5, 8};
+        assertArrayEquals(expected, actual);
+        System.out.println("testPositive passed");
+    }
+
+    @Test
+    public void testNegatives() {
+        SelectionSort sorter = new SelectionSort();
+        int[] actual = {-5, -3, -8, -4, -2};
+        sorter.selectionSort(actual);
+        int[] expected = {-8, -5, -4, -3, -2};
+        assertArrayEquals(expected, actual);
+        System.out.println("testNegatives passed");
+    }
+
+    @Test
+    public void testMixed() {
+        SelectionSort sorter = new SelectionSort();
+        int[] actual = {3, -2, 0, 5, -1};
+        sorter.selectionSort(actual);
+        int[] expected = {-2, -1, 0, 3, 5};
+        assertArrayEquals(expected, actual);
+        System.out.println("testMixed passed");
+    }
+}
